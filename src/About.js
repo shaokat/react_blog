22661f0d0b2e7abf4c2873d39f5147f41b0a288d
shaokat <shaokatcse@gmail.com>
2022-02-27 17:40:08 +0100

    initial commit

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+        <h2>About</h2>
+        <p style={{ marginTop: "1rem" }}>This blog app is a project to practive React js.</p>
+    </main>
+)
+}
+
+export default About
---
+++ src/PostPage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+
+const PostPage = ({posts, handleDelete}) => {
+  const { id } = useParams();
+  const post = posts.find(post => (post.id).toString() === id)
+  return (
+    <main className='PostPage'>
+      <article>
+        {post && 
+          <>
+            <h2>{post.title}</h2>
+            <p className='postDate'>{post.dateTime} </p>
+            <p className='postBody'>{post.body} </p>
+            <Link to={`/edit/${post.id}`}><button className="editButton">Edit Post</button></Link>
+            <button className='deleteButton' onClick={() => handleDelete(post.id)}>
+              Delete Post
+            </button>
+          </>
+        }
+        {!post &&
+        <>
+          <h2>Post Not Found !</h2>
+          <p> Well, that's disapointing.</p>
+          <p>
+            <Link to='/'> Visit Our Homepage</Link>
+          </p>
+        </>
+
+        }
+      </article>
+    </main>
+  )
+}
+
+export default PostPage
